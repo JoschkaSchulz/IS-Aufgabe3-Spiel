e05a363f144774af027638318014b3552d61bd33
Michael Hanisch <Michael.hanisch@haw-hamburg.de>
2014-05-19 10:52:41 +0200

    zeichnen angepasst

--- core/src/de/potoopirate/arena/game/Game.java
+++ core/src/de/potoopirate/arena/game/Game.java
@@ -15,33 +15,33 @@ import de.potoopirate.arena.player.Player;
 import de.potoopirate.arena.player.PlayersHUD;
 import de.potoopirate.arena.utils.ResourceLoader;
 
-public class Game implements Screen,IClockListener{
-	
+public class Game implements Screen, IClockListener {
+
 	public static final Vector2 POSITION_LEFT_FIGHT = new Vector2(507, 540);
 	public static final Vector2 POSITION_RIGHT_FIGHT = new Vector2(645, 540);
-	
+
 	private ShapeRenderer mDebugRenderer;
-	
+
 	private Fight mFight;
-	
+
 	private Clock mClock;
-	
+
 	private Player mPlayer1;
 	private Player mPlayer2;
 	private PlayersHUD mPlayersHud;
-	
+
 	private Arena mArena;
-	
+
 	public Game(Arena arena) {
 		mPlayer1 = new Player(Player.PLAYER_1);
 		mPlayer2 = new Player(Player.PLAYER_2);
 		mPlayersHud = new PlayersHUD(mPlayer1, mPlayer2);
-		
+
 		mClock = new Clock(this, mPlayer1, mPlayer2);
 		mFight = new Fight(this, mPlayer1, mPlayer2);
-		
+
 		mDebugRenderer = new ShapeRenderer();
-		
+
 		mArena = arena;
 	}
 
@@ -51,11 +51,11 @@ public class Game implements Screen,IClockListener{
 		mPlayer1.act(delta);
 		mPlayer2.act(delta);
 	}
-	
+
 	public void startClock() {
 		mClock.resumeClock();
 	}
-	
+
 	@Override
 	public void clockAction() {
 		mPlayer1.clockAction();
@@ -69,39 +69,46 @@ public class Game implements Screen,IClockListener{
 	@Override
 	public void render(float delta) {
 		Gdx.gl.glClearColor(0, 0, 0f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        
-        //act
-        this.act(delta); 
-        
-        //draw
-        mArena.getBatch().begin();
-        mArena.getBatch().draw(ResourceLoader.FIELD[0], 0, 0);
-        mArena.getBatch().draw(ResourceLoader.FIELD[1], 640, 0);
-        mArena.getBatch().draw(ResourceLoader.RING, 408, 450);
-        
-        //Draw Player
-        mPlayer1.draw(mArena.getBatch());
-        mPlayer2.draw(mArena.getBatch());
-        mFight.draw(mArena.getBatch());
-        
-		//HUD Backgrounds
-        mArena.getBatch().draw(ResourceLoader.LEFT_PLAYER_BACKGROUND, 0, 0);
-        mArena.getBatch().draw(ResourceLoader.RIGHT_PLAYER_BACKGROUND, 773, 0);
-        
-        //Players Hud
-        mPlayersHud.draw(mArena.getBatch());
-        
-        //Clock
-        mClock.draw(mArena.getBatch());
-        
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		// act
+		this.act(delta);
+
+		// draw
+		mArena.getBatch().begin();
+		mArena.getBatch().draw(ResourceLoader.FIELD[0], 0, 0);
+		mArena.getBatch().draw(ResourceLoader.FIELD[1],
+				ResourceLoader.FIELD[0].getRegionWidth(), 0);
+		mArena.getBatch().draw(ResourceLoader.RING, 408, 450);
+
+		// Draw Player
+		mPlayer1.draw(mArena.getBatch());
+		mPlayer2.draw(mArena.getBatch());
+		mFight.draw(mArena.getBatch());
+
+		// HUD Backgrounds
+		mArena.getBatch().draw(ResourceLoader.LEFT_PLAYER_BACKGROUND, 0, 0);
+		mArena.getBatch().draw(
+				ResourceLoader.RIGHT_PLAYER_BACKGROUND,
+				Gdx.graphics.getWidth()
+						- ResourceLoader.RIGHT_PLAYER_BACKGROUND
+								.getRegionWidth(), 0);
+
+		// Players Hud
+		mPlayersHud.draw(mArena.getBatch());
+
+		// Clock
+		mClock.draw(mArena.getBatch());
+
 		mArena.getBatch().end();
-		
-		//Debug Renderer
+
+		// Debug Renderer
 		mDebugRenderer.begin(ShapeType.Line);
 		mDebugRenderer.setColor(1f, 0f, 0f, 1f);
-		mDebugRenderer.rect(POSITION_LEFT_FIGHT.x, POSITION_LEFT_FIGHT.y, 128, 256);
-		mDebugRenderer.rect(POSITION_RIGHT_FIGHT.x, POSITION_RIGHT_FIGHT.y, 128, 256);
+		mDebugRenderer.rect(POSITION_LEFT_FIGHT.x, POSITION_LEFT_FIGHT.y, 128,
+				256);
+		mDebugRenderer.rect(POSITION_RIGHT_FIGHT.x, POSITION_RIGHT_FIGHT.y,
+				128, 256);
 		mDebugRenderer.end();
 	}
 
